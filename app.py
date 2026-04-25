f7c224a16b6fa2bd526517a4d99718f764caac07
Maria Abreu Andrade <daiane.andrade@thomsonreuters.com>
2026-04-25 12:09:35 -0300

    Fix: Remove deprecated preauthorized parameter from authenticator

--- app.py
+++ app.py
@@ -248,8 +248,7 @@ authenticator = stauth.Authenticate(
     config['credentials'],
     config['cookie']['name'],
     config['cookie']['key'],
-    config['cookie']['expiry_days'],
-    config['preauthorized']
+    config['cookie']['expiry_days']
 )
 
 # Login
